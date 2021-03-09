74887b66ccec0ba7d898683cce6eae59490c2230
Chandraa Mahadik <me8myneed@gmail.com>
2021-03-10 01:06:12 +0530

    Header Footer

--- eCommerce-App/frontend/src/App.js
+++ eCommerce-App/frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome To Deals Wagon.</h1>
+      <Header />
+      <main>
+        <h1>Welcome To Deals Wagon.</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
