680cd2dcab76c012e69f741f125e9b27a4e272bd
Chandraa Mahadik <me8myneed@gmail.com>
2021-03-22 09:22:09 +0530

    Backend Authentication

--- eCommerce-App/backend/controllers/userController.js
+++ eCommerce-App/backend/controllers/userController.js
@@ -33,12 +33,13 @@ const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
-    res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin
-      });
+    res.send("token received.")
+    // res.json({
+    //     _id: user._id,
+    //     name: user.name,
+    //     email: user.email,
+    //     isAdmin: user.isAdmin
+    //   });
   } else {
     res.status(404);
     throw new Error("User not found.");
--- eCommerce-App/backend/server.js
+++ eCommerce-App/backend/server.js
@@ -17,21 +17,25 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
+app.get("/", (req, res) => {
+  res.send("Api is set up.");
+});
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api is set up.");
-  });
-}
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Api is set up.");
+//   });
+// }
 
 app.use(notFound);
 
